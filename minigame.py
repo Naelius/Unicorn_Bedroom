9490e307f3cbee54a4f24ba0c8c1a34bb8a1b428
Gottcollo <gottcollo@gmail.com>
2025-06-04 13:17:51 +0200

    spielbearbeitet

--- minigame.py
+++ minigame.py
@@ -6,12 +6,38 @@ def klickerspiel():
     fenster.geometry('500x500')
 
     punktzahl = tk.IntVar(value=0)
+    zeit = tk.IntVar(value=10) #timer für 10 Sekunden
 
+    #punkteanzeige
     punktelabel = tk.Label(fenster, textvariable=punktzahl, font=('Gothic', 14))
     punktelabel.pack(pady=20)
+
+    #timeranzeige
+    zeitlabel = tk.Label(fenster, text=F'Zeit: {zeit.get()} Sekunden', font=('Gothic', 14))
+    zeitlabel.pack(pady=10)
 
     def klick():
         punktzahl.set(punktzahl.get() + 1)
 
     klickbutton = tk.Button(fenster, text='Klickerhero', command=klick, font=('Gothic', 14))
     klickbutton.pack(pady=10)
+
+    def countdown():
+        if zeit.get() > 0:
+            zeit.set(zeit.get() - 1)
+            zeitlabel.config(text=F'Zeit: {zeit.get()} Sekunden')
+            fenster.after(1000, countdown) #1sekunde ist 1000ms 
+        else:
+            klickbutton.config(state='disabled')
+            zeitlabel.config(text=F'Over! Heroclicks: {punktzahl.get()}')
+    
+    def start_spiel():
+        punktzahl.set(0)
+        zeit.set(10)
+        zeitlabel.config(text=F'Zeit: {zeit.get()} Sekunden')
+        klickbutton.config(state='normal')
+        countdown()
+
+    
+    restart_button = tk.Button(fenster, text='Ready?', command=start_spiel, font=('Gothic', 20))
+    restart_button.pack(pady=20)
