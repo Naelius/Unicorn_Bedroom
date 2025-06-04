689b7730bc5c3d30dca6932175e8cd11fcd6c3af
Gottcollo <gottcollo@gmail.com>
2025-06-04 12:55:20 +0200

    funktioniertes spiel

--- minigame.py
+++ minigame.py
@@ -7,11 +7,11 @@ def klickerspiel():
 
     punktzahl = tk.IntVar(value=0)
 
-    punktelabel = tk.Label(fenster, textvariable=punktzahl, font=(Gothic, 14))
+    punktelabel = tk.Label(fenster, textvariable=punktzahl, font=('Gothic', 14))
     punktelabel.pack(pady=20)
 
     def klick():
         punktzahl.set(punktzahl.get() + 1)
 
-    klickbutton = tk.Button(fenster, text='Klickerhero', command=klick, font=(Gothic, 14))
+    klickbutton = tk.Button(fenster, text='Klickerhero', command=klick, font=('Gothic', 14))
     klickbutton.pack(pady=10)
